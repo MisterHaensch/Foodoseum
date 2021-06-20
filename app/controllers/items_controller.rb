61f2ad8c9802cc6e08742ae3e5f4eb0fc26d0633
Martin <haenschmartin@gmail.com>
2021-06-19 22:55:49 -0700

    Add new and create Action for Item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,17 @@ class ItemsController < ApplicationController
     def index
     end
 
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+    end
+
+    private
     
+    def item_prams
+        params.require(:item).permit(:title, :description)
+    end
 end
