20ab0f525ae9b2ecd7362919b04a048254b9877e
Martin <haenschmartin@gmail.com>
2021-06-21 22:17:36 -0700

    Add Category Model and made connection between category and user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_061811) do
+ActiveRecord::Schema.define(version: 2021_06_22_050045) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "string"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -18,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_061811) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
