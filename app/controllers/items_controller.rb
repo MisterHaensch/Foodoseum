b3d75f231692a8640324ee874b097fac8d1755fc
Martin <haenschmartin@gmail.com>
2021-06-25 00:23:41 -0700

    print items by category on index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,51 @@ class ItemsController < ApplicationController
     before_action :find_item, only: [:show, :edit, :update, :destroy]  
     #übersetzt: hiermit wird sichergestellt, dass die action nur von show, edit, update und destroy benutzt werden dürfen.
 
-    def index
+    def test
         if params[:category].blank?
-            @items = Item.all.order("created_at DESC")
+            #@items = Item.all.order("created_at DESC")
+            @categories = Category.all
+            @all_items = {}
+            @categories.each do |cat|
+                temp = Item.where(:category_id => cat).order("title ASC")
+                @all_items[cat] = temp
+
+            end
+            
+            #@category_id = Category.find_by(name: "Drinks").id
+            #@items_drinks = Item.where(:category_id => @category_id).order("title ASC")
+
+            #@category_id = Category.find_by(name: "Cleaning Agent").id
+            #@items_cleaning = Item.where(:category_id => @category_id).order("title ASC")
+
+            #@category_id = Category.find_by(name: "Frozen Food").id
+            #@items_frozen = Item.where(:category_id => @category_id).order("title ASC")
+
+            #@category_id = Category.find_by(name: "Cookies").id
+            #@#items_cookies = Item.where(:category_id => @category_id).order("title ASC")
+
+            #@category_id = Category.find_by(name: "Beauty").id
+            #@items_beauty = Item.where(:category_id => @category_id).order("title ASC")
+
+            #@category_id = Category.find_by(name: "Ice Cream").id
+            #@items_ice = Item.where(:category_id => @category_id).order("title ASC")
+
         else
             @category_id = Category.find_by(name: params[:category]).id
             @items = Item.where(:category_id => @category_id).order("created_at DESC")
         end
+
+    end 
+
+
+    def index
+        @categories = Category.all
+            @all_items = Hash.new(0)
+            @categories.each do |cat|
+                temp = Item.where(:category_id => cat).order("title ASC")
+                @all_items[cat.name] = temp
+
+            end
     end
 
     def show        
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+
   devise_for :users
   resources :items
   root 'items#index'
   resources :categories
+
+
 end
